f4d7cc3afb99cfa5db407dc76ca45caa9f087653
ScubaGurl <amcassel@yahoo.com>
2026-03-31 12:47:16 -0400

    Remove 2 test comments from other 2 team memnbers setting up git

--- DialogicWrapper.cs
+++ DialogicWrapper.cs
@@ -6,8 +6,6 @@ public partial class DialogicWrapper : Node {
 	public override void _Ready() {
 		 Dialogic = GetNode<Node>("/root/Dialogic");
 	}
-	// test comment 1
-	// test comment 2
 
 	public void Start(string scene) {
 		Dialogic.Call("start", scene);
